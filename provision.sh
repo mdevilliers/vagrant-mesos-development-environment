5a15877cd93d35b4f2072aa761cc18ef836ca717
Mark deVilliers <markdevilliers@gmail.com>
2014-01-16 11:37:37 +0000

    Moved source to /home directory

--- provision.sh
+++ provision.sh
@@ -5,7 +5,9 @@ apt-get update > /dev/null
 
 apt-get -y install make git gcc g++ python-dev libcppunit-dev libunwind7-dev openjdk-7-jdk autoconf autotools-dev libltdl-dev libtool autopoint libcurl4-openssl-dev libsasl2-dev
 
-cd /vagrant
+cd /home
+mkdir mesos-dev
+cd mesos-dev
 
 git clone git://git.apache.org/mesos.git
 

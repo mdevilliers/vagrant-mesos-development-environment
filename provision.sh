34506e6ef5ddb961c2676db9d1b4081b64c427fd
Mark deVilliers <markdevilliers@gmail.com>
2014-06-03 15:32:18 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -18,6 +18,7 @@ apt-get upgrade > /dev/null
 apt-get -y install make git gcc g++ curl  
 apt-get -y install python-dev libcppunit-dev libunwind7-dev autoconf autotools-dev libltdl-dev libtool autopoint libcurl4-openssl-dev libsasl2-dev
 apt-get -y install openjdk-7-jdk zookeeperd default-jre python-setuptools python-protobuf
+apt-get -y install libprotobuf-dev protobuf-compiler
 apt-get -y --force-yes install esl-erlang
 
 curl -sSfL http://downloads.mesosphere.io/master/ubuntu/12.10/mesos_0.18.0-rc5_amd64.deb --output /tmp/mesos.deb

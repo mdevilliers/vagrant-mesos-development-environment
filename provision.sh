d25bc3b395a20b692f21f72cc12a773d17c4e423
Mark deVilliers <markdevilliers@gmail.com>
2015-11-02 14:10:29 +0000

    fix java 8 install

--- provision.sh
+++ provision.sh
@@ -18,13 +18,15 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E9
 
 echo "deb https://get.docker.io/ubuntu docker main" | sudo tee /etc/apt/sources.list.d/docker.list
 
+# java 8
+add-apt-repository -y ppa:openjdk-r/ppa
 
 apt-get -y update > /dev/null
 apt-get upgrade > /dev/null
 
 apt-get -y install make git gcc g++ curl  
 apt-get -y install python-dev libcppunit-dev libunwind7-dev autoconf autotools-dev libltdl-dev libtool autopoint libcurl4-openssl-dev libsasl2-dev
-apt-get -y install openjdk-7-jdk zookeeperd default-jre python-setuptools python-protobuf
+apt-get -y install openjdk-8-jdk zookeeperd default-jre python-setuptools python-protobuf
 apt-get -y install libprotobuf-dev protobuf-compiler
 apt-get -y install erlang
 apt-get -y install mesos marathon
